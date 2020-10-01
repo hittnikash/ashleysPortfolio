d141aa1cc22aa5a4d03b1f19b62729d30536b5e2
Ashley Needy <ashleyneedy@yahoo.com>
2020-10-01 04:49:03 -0400

    attempting to place a hamburger and change background hover image of navigation  menu

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 /* Hamburger Menu */
-const hamburger = document.getElementById('hamburger');
-const navUL = document.getElementById('nav-ul');
 
-hamburger.addEventListener('click', () => {
-        navUL.classList.toggle('show');
+function toggle() {
+    let menu = document.getElementById("nav-ul");
+        if (menu.style.display === "block") {
+            menu.style.display = "none";     
+        }
+        else {
+           menu.style.display = "block";
+        }
+}
+
+document.getElementById("hamburger").addEventListener("click", "toggle");
+
+
+
+
+/* Mouse-Over Nav */
+
+$("nav-ul").mouseover(function() {
+        $("nav-ul").css("background-image",  url("img/icons/hoverTextureVec.png"));
 });
 

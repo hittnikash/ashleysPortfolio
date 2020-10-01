45dc5ee3af2216714d0268204bce939b5b7fbd48
Ashley Needy <ashleyneedy@yahoo.com>
2020-09-30 23:40:34 -0400

    started working on mobile navigation- hamburger

---
+++ app.js
@@ -0,0 +1,8 @@
+/* Hamburger Menu */
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+        navUL.classList.toggle('show');
+});
+
